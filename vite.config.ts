fbb8c740de6417ad967ad66fcad368cedfd611c5
ensunluer <ensunluer@gmail.com>
2024-08-31 17:09:07 +0300

    chore(refactor): clear and adjust config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(),
+    svgr({
+      // svgr options: https://react-svgr.com/docs/options/
+      svgrOptions: { exportType: "default", ref: true, svgo: false, titleProp: true },
+      include: "**/*.svg",
+      exclude: ['src/assets/img/**/*.svg']
+    }),],
+});
